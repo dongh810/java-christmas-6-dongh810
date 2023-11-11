b72d3ba94d2354f0b7189719391a84f971812192
dongh810 <baek0810@naver.com>
2023-11-12 03:46:59 +0900

    feat: 각 메뉴의 개수 추가

---
+++ src/main/java/christmas/constant/MenuCount.java
@@ -0,0 +1,16 @@
+package christmas.constant;
+
+public class MenuCount {
+    public int mainCount;
+    public int epitigerCount;
+    public int drinkCount;
+    public int dessertCount;
+
+    public MenuCount(int mainCount, int dessertCount, int drinkCount, int epitigerCount) {
+        this.dessertCount =dessertCount;
+        this.drinkCount = drinkCount;
+        this.epitigerCount = epitigerCount;
+        this.mainCount = mainCount;
+    }
+
+}
