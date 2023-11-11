f798b748f8cb1dd8081ede805d2ea3afc4b7b10b
dongh810 <baek0810@naver.com>
2023-11-12 03:45:04 +0900

    feat: 진행메시지 추가

---
+++ src/main/java/christmas/constant/ProgressMessage.java
@@ -0,0 +1,26 @@
+package christmas.constant;
+
+public class ProgressMessage {
+
+    public static final String START_HELLO = "안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.";
+
+    public static final String INPUT_VISIT_DATE = "12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)";
+
+    public static final String INPUT_MENU_AND_COUNT = "주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)";
+
+    public static final String PRINT_EVENT = "12월 %d일에 우테코 식당에서 받을 이벤트 혜택 미리보기!";
+
+    public static final String PRINT_ORDER_MENU = "<주문 메뉴>";
+
+    public static final String PRINT_BEFORE_DISCOUNT = "<할인 전 총주문 금액>";
+
+    public static final String PRINT_GIVING_MENU = "<증정 메뉴>";
+
+    public static final String PRINT_EVENT_THINGS = "<혜택 내역>";
+
+    public static final String PRINT_DISCOUNT = "<총혜택 금액>";
+
+    public static final String PRINT_AFTER_DISCOUNT = "<할인 후 예상 결제 금액>";
+
+    public static final String EVENT_BADGE = "<12월 이벤트 베지>";
+}
