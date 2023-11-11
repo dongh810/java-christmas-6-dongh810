f825fabbd248a4590ab578ba1c713c01a4fdda12
dongh810 <baek0810@naver.com>
2023-11-12 03:52:15 +0900

    feat: 날짜에 관한 기능 추가
    평일,주말,특별한날 이렇게 세가지로 구분하여
    날짜를 입력받았을때 무슨날인지 구분하는 기능 추가.

---
+++ src/main/java/christmas/domain/ManageCalendar.java
@@ -0,0 +1,27 @@
+package christmas.domain;
+
+import christmas.constant.Calendar;
+
+import java.util.List;
+
+public class ManageCalendar {
+
+    Calendar calendar = new Calendar();
+    List<Integer> weekday = calendar.setWeekDay();
+    List<Integer> weekend = calendar.setWeekend();
+    List<Integer> specialDay = calendar.setSpecialDay();
+
+    public int getDate(int day) {
+        int visitDay = day;
+        if(weekday.contains(visitDay)){
+            return 1;
+        }
+        if(weekend.contains(visitDay)) {
+            return 2;
+        }
+        if(specialDay.contains(visitDay)) {
+            return 3;
+        }
+        return 0;
+    }
+}
