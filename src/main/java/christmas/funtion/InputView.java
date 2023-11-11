8808ff54203726ef3f21eb82a0ce09f2181c971f
dongh810 <baek0810@naver.com>
2023-11-12 03:52:41 +0900

    feat: 입력을 담당하는 기능 추가
    날짜와 메뉴를 받아오는 기능 추가.

---
+++ src/main/java/christmas/funtion/InputView.java
@@ -0,0 +1,18 @@
+package christmas.funtion;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+    public int readDate() {
+        String getDate = Console.readLine();
+        int date = Integer.parseInt(getDate);
+        return date;
+    }
+
+    public String readMenu() {
+        String getMenu = Console.readLine();
+        return getMenu;
+    }
+
+
+}
