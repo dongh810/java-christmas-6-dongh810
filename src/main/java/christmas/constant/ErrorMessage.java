bd59d5bf0e047c377cc22bad7281bbd2902cd39c
dongh810 <baek0810@naver.com>
2023-11-12 03:44:48 +0900

    feat: 에러메시지 추가

---
+++ src/main/java/christmas/constant/ErrorMessage.java
@@ -0,0 +1,12 @@
+package christmas.constant;
+
+public class ErrorMessage {
+
+    public static final String NO_ONLY_DRINK = "[ERROR] 음료만 주문이 불가능합니다.";
+
+    public static final String MENU_ORDER_LIMIT = "[ERROR] 메뉴는 20개이상 주문이 불가능합니다.";
+
+    public static final String WRONG_DATE = "[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.";
+
+    public static final String WRONG_ORDER = "[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.";
+}
