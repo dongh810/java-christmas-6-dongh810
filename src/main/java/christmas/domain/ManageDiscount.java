c720cb457d6e3d02dc2da061c46725580729b8e1
dongh810 <baek0810@naver.com>
2023-11-13 03:15:22 +0900

    feat: 할인을 관리하는 기능 추가
    모든 할인을 리스트로 받아오는 기능과,
    값으로 더해서 가져오는 기능 추가.

--- src/main/java/christmas/domain/ManageDiscount.java
+++ src/main/java/christmas/domain/ManageDiscount.java
@@ -1,46 +1,70 @@
 package christmas.domain;
 
-import christmas.constant.Calendar;
-import christmas.constant.MenuCount;
-
+import java.util.ArrayList;
 import java.util.List;
 
 public class ManageDiscount {
 
-    Calendar calendar = new Calendar();
     ManageMenu manageMenu = new ManageMenu();
-    List<Integer> weekday = calendar.setWeekDay();
-    List<Integer> weekend = calendar.setWeekend();
-    List<Integer> specialDay = calendar.setSpecialDay();
-    Integer discount = 0;
 
-    public void getDiscount(int divisionNum,String inputMenu) {
-        MenuCount menuCount = manageMenu.countEachMenuForDiscount(inputMenu);
-        int dessertNumber = menuCount.dessertCount; // 디저트개수 가져와야함
-        int mainNumber = menuCount.mainCount; //메인개수 가져와야함
-        if(divisionNum == 1){
-             discount += dessertNumber * 2023;
+    public List<Integer> setAllDiscount(int divisionNum, String inputMenu) {
+        int dessertNumber = manageMenu.getAllDessertCount(inputMenu);
+        int mainNumber = manageMenu.getAllMainCount(inputMenu);
+        Integer weekdayDiscount = 0;
+        Integer weekendDiscount = 0;
+        Integer specialDiscount = 0;
+        List<Integer> allDiscount = new ArrayList<>();
+        if (divisionNum == 1) {
+            weekdayDiscount += dessertNumber * 2023;
+            allDiscount.add(weekdayDiscount);
         }
-        if(divisionNum == 2) {
-             discount += mainNumber * 2023;
+        if (divisionNum == 2) {
+            weekendDiscount += mainNumber * 2023;
+            allDiscount.add(weekendDiscount);
         }
-        if(divisionNum == 3) {
-            discount += 1000;
+        if (divisionNum == 3) {
+            weekdayDiscount += dessertNumber * 2023;
+            specialDiscount += 1000;
+            allDiscount.add(weekdayDiscount);
+            allDiscount.add(specialDiscount);
         }
+        return allDiscount;
     }
 
     public int getDayDiscount(int day) {
         int dayDiscount = 1000;
-        if(day<=25){
-            for(int i =1; i<day+1 ; i++){
+        if (day <= 25) {
+            for (int i = 1; i < day; i++) {
                 dayDiscount += 100;
             }
         }
-        if(day>25){
+        if (day > 25) {
             dayDiscount = 0;
         }
         return dayDiscount;
     }
 
+    public List<Integer> getAllDiscount(int divisionNum, String inputMenu, int day) {
+        List<Integer> allDiscount = setAllDiscount(divisionNum, inputMenu);
+        if (allDiscount.size() == 1) {
+            if (allDiscount.get(0) == 0) {
+                allDiscount.clear();
+            }
+        }
+        if (allDiscount.size() == 2) {
+            if (allDiscount.get(0) == 0 && allDiscount.get(1) == 0) {
+                allDiscount.clear();
+            }
+        }
+        return allDiscount;
+    }
 
+    public int getTotalDiscount(int divisionNum, String inputMenu, int day) {
+        List<Integer> discount = getAllDiscount(divisionNum, inputMenu, day);
+        int totalDiscount = 0;
+        for (int i = 0; i < discount.size(); i++) {
+            totalDiscount += discount.get(i);
+        }
+        return totalDiscount;
+    }
 }
