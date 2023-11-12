e6cf39b9a02436bf9ec95988b2039220dce8dff1
dongh810 <baek0810@naver.com>
2023-11-13 03:12:22 +0900

    refactor: 캘린더관련 기능 코드정리

--- src/main/java/christmas/constant/Calendar.java
+++ src/main/java/christmas/constant/Calendar.java
@@ -6,8 +6,8 @@ import java.util.List;
 public class Calendar {
     public List<Integer> setWeekDay() {
         List<Integer> weekday = new ArrayList<>();
-        Integer[] weekdayNumber = {4,5,6,7,11,12,13,14,18,19,20,21,26,27,28};
-        for(int i=0;i< weekdayNumber.length;i++){
+        Integer[] weekdayNumber = {4, 5, 6, 7, 11, 12, 13, 14, 18, 19, 20, 21, 26, 27, 28};
+        for (int i = 0; i < weekdayNumber.length; i++) {
             weekday.add(weekdayNumber[i]);
         }
         return weekday;
@@ -15,8 +15,8 @@ public class Calendar {
 
     public List<Integer> setWeekend() {
         List<Integer> weekend = new ArrayList<>();
-        Integer[] weekendNumber = {1,2,8,9,15,16,22,23,29,30};
-        for(int i=0;i< weekendNumber.length;i++){
+        Integer[] weekendNumber = {1, 2, 8, 9, 15, 16, 22, 23, 29, 30};
+        for (int i = 0; i < weekendNumber.length; i++) {
             weekend.add(weekendNumber[i]);
         }
         return weekend;
@@ -24,8 +24,8 @@ public class Calendar {
 
     public List<Integer> setSpecialDay() {
         List<Integer> specialDay = new ArrayList<>();
-        Integer[] specialDayNumber = {3,10,17,24,25,31};
-        for(int i=0;i< specialDayNumber.length;i++){
+        Integer[] specialDayNumber = {3, 10, 17, 24, 25, 31};
+        for (int i = 0; i < specialDayNumber.length; i++) {
             specialDay.add(specialDayNumber[i]);
         }
         return specialDay;
