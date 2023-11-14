6cd0c7b5eeeb610df8c3c479a91e94c37b442d68
dongh810 <baek0810@naver.com>
2023-11-15 05:54:31 +0900

    refactor: 검증기능 추가
    검증기능을 입력받을때 사용하도록 코드 추가.

--- src/main/java/christmas/funtion/InputView.java
+++ src/main/java/christmas/funtion/InputView.java
@@ -3,14 +3,20 @@ package christmas.funtion;
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
+
+    ValidationFunction validationFunction = new ValidationFunction();
+
     public int readDate() {
         String getDate = Console.readLine();
+        validationFunction.validateDateNumber(getDate);
         int date = Integer.parseInt(getDate);
+        validationFunction.validateDateRange(date);
         return date;
     }
 
     public String readMenu() {
         String getMenu = Console.readLine();
+        validationFunction.validateAllOrderCondition(getMenu);
         return getMenu;
     }
 }
