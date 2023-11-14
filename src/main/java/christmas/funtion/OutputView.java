559863e9bde1d045611cbb2114486ce228bf4f17
dongh810 <baek0810@naver.com>
2023-11-15 05:52:42 +0900

    refactor: 함수분리 및 숫자출력형식 추가
    함수의 크기를 줄이기 위해 함수를 새로 추가하고,
    돈을 출력하기 위해 숫자출력 형식 추가.

--- src/main/java/christmas/funtion/OutputView.java
+++ src/main/java/christmas/funtion/OutputView.java
@@ -3,6 +3,8 @@ package christmas.funtion;
 import christmas.constant.ProgressMessage;
 import christmas.domain.*;
 
+import java.text.DecimalFormat;
+import java.util.ArrayList;
 import java.util.List;
 
 public class OutputView {
@@ -15,7 +17,6 @@ public class OutputView {
 
     public void printMenu() {
         System.out.println(ProgressMessage.INPUT_MENU_AND_COUNT);
-
     }
 
     public void printHello() {
@@ -41,7 +42,8 @@ public class OutputView {
 
     public void printBeforDiscount(String inputMenu) {
         int totalPrice = managePrice.getTotalPrice(inputMenu);
-        System.out.println("\n" + ProgressMessage.PRINT_BEFORE_DISCOUNT + "\n" + totalPrice + "원");
+        String result = printNumberComma(totalPrice);
+        System.out.println("\n" + ProgressMessage.PRINT_BEFORE_DISCOUNT + "\n" + result + "원");
     }
 
     public void printGift(String inputMenu) {
@@ -56,41 +58,47 @@ public class OutputView {
 
     public void printEventThings(int divisionNum, String inputMenu, int day) {
         List<Integer> discount = manageDiscount.getAllDiscount(divisionNum, inputMenu, day);
+        List<String> allResult = printNumbersComma(discount);
         int totalPrice = managePrice.getTotalPrice(inputMenu);
         System.out.println("\n" + ProgressMessage.PRINT_EVENT_THINGS);
         printChristmasEvent(divisionNum, inputMenu, day);
-        if (divisionNum == 1 && discount.size() != 0) {
-            System.out.println("평일 할인: -" + discount.get(0) + "원");
-        }
-        if (divisionNum == 2 && discount.size() != 0) {
-            System.out.println("주말 할인: -" + discount.get(0) + "원");
-        }
-        if (divisionNum == 3 && discount.size() != 0) {
-            if (discount.get(0) != 0) {
-                System.out.println("평일 할인: -" + discount.get(0) + "원");
-                System.out.println("특별 할인: -" + discount.get(1) + "원");
-            }
-            if (discount.get(0) == 0) {
-                System.out.println("특별 할인: -" + discount.get(1) + "원");
-            }
-        }
+        printEventCondition(divisionNum, discount, allResult);
         printEvent(inputMenu);
         if (discount.size() == 0 && totalPrice <= 120000 && day >= 26) {
             System.out.println("없음");
         }
     }
 
+    public void printEventCondition(int divisionNum, List<Integer> discount, List<String> allResult) {
+        if (divisionNum == 1 && discount.size() != 0) {
+            System.out.println("평일 할인: -" + allResult.get(0) + "원");
+        }
+        if (divisionNum == 2 && discount.size() != 0) {
+            System.out.println("주말 할인: -" + allResult.get(0) + "원");
+        }
+        if (divisionNum == 3 && discount.size() != 0) {
+            if (discount.get(0) != 0) {
+                System.out.println("평일 할인: -" + allResult.get(0) + "원");
+                System.out.println("특별 할인: -" + allResult.get(1) + "원");
+            }
+            if (discount.get(0) == 0) {
+                System.out.println("특별 할인: -" + allResult.get(1) + "원");
+            }
+        }
+    }
+
     public void printChristmasEvent(int divisionNum, String inputMenu, int day) {
         int discount = manageDiscount.getDayDiscount(day);
+        String discountResult = printNumberComma(discount);
         if (day < 26) {
-            System.out.println("크리스마스 디데이 할인: -" + discount + "원");
+            System.out.println("크리스마스 디데이 할인: -" + discountResult + "원");
         }
     }
 
     public void printEvent(String inputMenu) {
         int totalPrice = managePrice.getTotalPrice(inputMenu);
         if (totalPrice >= 120000) {
-            System.out.println("증정 이벤트: -25000원");
+            System.out.println("증정 이벤트: -25,000원");
         }
     }
 
@@ -100,20 +108,23 @@ public class OutputView {
         if (totalPrice >= 120000) {
             discount = discount + 25000;
         }
+        String discountResult = printNumberComma(discount);
         System.out.println("\n" + ProgressMessage.PRINT_DISCOUNT);
         if (discount == 0) {
-            System.out.println(discount + "원");
+            System.out.println(discountResult + "원");
         }
         if (discount != 0) {
-            System.out.println("-" + discount + "원");
+            System.out.println("-" + discountResult + "원");
         }
     }
 
     public void printTotalPay(int divisionNum, String inputMenu, int day) {
         int discount = manageDiscount.getTotalDiscount(divisionNum, inputMenu, day);
         int totalPrice = managePrice.getTotalPrice(inputMenu);
+        int result = totalPrice - discount;
+        String resultWithComma = printNumberComma(result);
         System.out.println("\n" + ProgressMessage.PRINT_AFTER_DISCOUNT);
-        System.out.println(totalPrice - discount + "원");
+        System.out.println(resultWithComma + "원");
     }
 
     public void printBedge(int divisionNum, String inputMenu, int day) {
@@ -123,5 +134,20 @@ public class OutputView {
 
     public void printLineChange() {
         System.out.println("\n");
+    }
+
+    public String printNumberComma(int result) {
+        DecimalFormat df = new DecimalFormat("###,###");
+        String money = df.format(result);
+        return money;
+    }
+
+    public List<String> printNumbersComma(List<Integer> result) {
+        DecimalFormat df = new DecimalFormat("###,###");
+        List<String> allResult = new ArrayList<>();
+        for (int i = 0; i < result.size(); i++) {
+            allResult.add(df.format(result.get(i)));
+        }
+        return allResult;
     }
 }
